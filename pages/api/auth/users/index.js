9e75916abb34c3e987abe72297603477268cdb34
NoahBenschine <noahbenschine@gmail.com>
2021-11-17 07:08:38 -0500

    Changed auth/user hopefully fixes vercel

--- pages/api/auth/users/index.js
+++ pages/api/auth/users/index.js
@@ -17,23 +17,23 @@
 //       })
 //     );
 //   }
-// };
-import dbConnect from "../../../utils/dbConnect";
-
-export default function dbAdd(){
-  const db = dbConnect(); // retrieve db instance
-
-  // you can also use switch/case on req.method for a clean structure
-
-  	// query db for users collection
-    db.collection("users", (err, usersCollection) =>
-
-      // on retrieval of the users collection run any desired query methods to the collection
-      usersCollection.find({}).toArray((err, users) => {
-
-      // respond with users as json
-        res.status(200).json(users);
-
-      })
-    );
-}
+// // };
+// import dbConnect from "../../../utils/dbConnect";
+//
+// export default function dbAdd(){
+//   const db = dbConnect(); // retrieve db instance
+//
+//   // you can also use switch/case on req.method for a clean structure
+//
+//   	// query db for users collection
+//     db.collection("users", (err, usersCollection) =>
+// 
+//       // on retrieval of the users collection run any desired query methods to the collection
+//       usersCollection.find({}).toArray((err, users) => {
+//
+//       // respond with users as json
+//         res.status(200).json(users);
+//
+//       })
+//     );
+// }
