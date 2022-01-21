e800d92a0fa6f5fa9138f5b7989961cabd720668
NoahBenschine <noahbenschine@gmail.com>
2022-01-20 22:44:24 -0500

    working on beautifier

--- src/user.js
+++ src/user.js
@@ -1,19 +1,21 @@
 const prisma = require("./prismaClient");
 
-async function getUserId(user_name){
- const user = await prisma.users.findFirst({
-     where: {
-       name: user_name
-     },
-   })
-   return user.id;
+async function getUserId(user_name) {
+    const user = await prisma.users.findFirst({
+        where: {
+            name: user_name
+        },
+    })
+    return user.id;
 }
 
-async function getAllUserIds(user_name){
- const user = await prisma.users.findMany({
-   })
-   return user.id;
- }
+async function getAllUserIds(user_name) {
+    const user = await prisma.users.findMany({})
+    return user.id;
+}
 
 
-export {getUserId,getAllUserIds};
+export {
+    getUserId,
+    getAllUserIds
+};
