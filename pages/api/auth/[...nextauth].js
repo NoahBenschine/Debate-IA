5697656505bda5de446514b2ccc11f66c9837fb5
NoahBenschine <noahbenschine@gmail.com>
2022-02-01 22:55:02 -0500

    fixed bad callbackurl working on fixing admin signin

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -25,16 +25,17 @@ export default async function auth(req, res){
       },
       callbacks: {
         async signIn({ user, account, profile, email, credentials }) {
-
-                    const isAllowedToSignIn = true
-             if (isAllowedToSignIn) {
-               return "/choosing/main";
-             } else {
-               // Return false to display a default error message
-               return false
-               // Or you can return a URL to redirect to:
-               // return '/unauthorized'
-             }
+          console.log(user);
+          const admin = await getAdminByUser(user.id);
+              console.log(admin);
+              if(admin == null && email == "tgetman@pvcsd.org"){
+                adminInsert(user.id);
+                  return "/admin/ControlPanel";
+              }else if(admin != null){
+                  return "/admin/ControlPanel";
+              }else{
+                return "/choosing/main";
+              }
 
 
  }
