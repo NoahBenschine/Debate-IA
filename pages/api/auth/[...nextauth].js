b6d556efb69a231fcd995523741e85ec7cafc188
NoahBenschine <noahbenschine@gmail.com>
2022-02-07 17:23:04 -0500

    changed signin link hopefully

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -33,7 +33,7 @@ export default async function auth(req, res){
           }else if(admin != null){
               return "/admin/ControlPanel";
           }else{
-            return true;
+            return "/adS/ControlPanel";
           }
         }
 }
