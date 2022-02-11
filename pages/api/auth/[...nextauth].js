2e1a780887a3e0134d61804fe8a48689882019c7
NoahBenschine <noahbenschine@gmail.com>
2022-02-10 21:51:27 -0500

    reverted session error still not fixed

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -18,17 +18,12 @@ export default async function auth(req, res){
     session: {
         // Seconds - How long until an idle session expires and is no longer valid.
          maxAge: 60*60, // 30 days
-<<<<<<< HEAD
-         strategy:"jwt"
-=======
->>>>>>> dev
         // Seconds - Throttle how frequently to write to database to extend a session
         // Use it to limit write operations. Set to 0 to always update the database.
         // Note: This option is ignored if using JSON Web Tokens
         // updateAge: 24 * 60 * 60, // 24 hours
       },
       callbacks: {
-<<<<<<< HEAD
         async session({ session, user, token }) {
           console.log("session was called");
           console.log(session);
@@ -51,23 +46,6 @@ export default async function auth(req, res){
             return "/choosing/main";
           }
         },
-=======
-        // async signIn({ user, account, profile, email, credentials }) {
-        //
-        //
-        //
-        //   const admin = await getAdminByUser(user.id);
-        //   console.log(admin);
-        //   if(admin == null && email == "tgetman@pvcsd.org"){
-        //     adminInsert(user.id);
-        //       return "/admin/ControlPanel";
-        //   }else if(admin != null){
-        //       return "/admin/ControlPanel";
-        //   }else{
-        //     return "/choosing/main";
-        //   }
-        // },
->>>>>>> dev
 
     async jwt({ token, account }) {
       console.log("jwt was called")
